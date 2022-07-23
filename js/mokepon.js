f6b8ccd9b9a6a0932977e7f8bd5044854cd8630b
jesusLayton <jesuslayton92@gmail.com>
2022-07-23 02:28:02 -0500

    actualizacion

--- js/mokepon.js
+++ js/mokepon.js
@@ -4,14 +4,21 @@ function iniciarJuego(){
 
 }
 function seleccionarMascotaJugador(){
-    if(document.getElementById('hipodoge').checked){
-        alert ('hipodoge')
-    }else if(document.getElementById('capipepo').checked){
-        alert('capipepo')
-    }else if(document.getElementById('ratigueya').checked){
-        alert('ratigueya')
+    let inputHipodoge = document.getElementById('hipodoge');
+    let inputCapipepo = document.getElementById('capipepo');
+    let inputRatigueya = document.getElementById('ratigueya');
+    let spanMascotaJugador = document.getElementById('mascota-jugador');
+    if(inputHipodoge.checked){
+        alert ('seleccionaste a hipodoge');
+        spanMascotaJugador.innerText='Hipodoge';
+    }else if(inputCapipepo.checked){
+        alert('seleccionaste a capipepo');
+        spanMascotaJugador.innerHTML='Capipepo';
+    }else if(inputRatigueya.checked){
+        alert('seleccionaste a ratigueya');
+        spanMascotaJugador.innerHTML='Ratigueya';
     }else{
-        alert('no se seleciono mascota')
+        alert('no se seleciono mascota');
     }
 }
 
